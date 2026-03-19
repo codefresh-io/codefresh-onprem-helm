d485e1c6f604f0cb1bf948f1e3b18bc735c54b2d
vitaliichyrka <vitalii.chyrka@codefresh.io>
2026-03-19 15:05:47 +0200

    fix script get-all-images.sh

--- scripts/get-all-images.sh
+++ scripts/get-all-images.sh
@@ -6,7 +6,7 @@ OUTPUTFILE=$1
 
 helm dep update $CHARTDIR
 
-helm template release-name $CHARTDIR -f $VALUESFILE \
+helm template release-name $CHARTDIR -f \
   | grep -E 'image:' | grep -v "{}" \
   | awk -F ': ' '{print $2}' | awk NF \
   | tr -d '"' | tr -d ',' | cut -f1 -d"@" \
