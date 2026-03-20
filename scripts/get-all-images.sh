825591293ca1f744528eaaa945abc67f7b552f0e
vitalii-codefresh <vitalii.chyrka@codefresh.io>
2026-03-20 08:43:12 +0200

    Update get-all-images.sh

--- scripts/get-all-images.sh
+++ scripts/get-all-images.sh
@@ -5,7 +5,7 @@ OUTPUTFILE=$1
 
 helm dep update $CHARTDIR
 
-helm template release-name $CHARTDIR -f \
+helm template release-name $CHARTDIR \
   | grep -E 'image:' | grep -v "{}" \
   | awk -F ': ' '{print $2}' | awk NF \
   | tr -d '"' | tr -d ',' | cut -f1 -d"@" \
