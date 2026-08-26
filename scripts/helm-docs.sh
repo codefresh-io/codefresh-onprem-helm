d5f3981d0f49be61c5b39a0b7fcf0d7ab52ab7dd
Bohdan Pysarenko <bohdan.pisarenko@octopus.com>
2026-08-26 14:53:32 +0300

    merg chart updates

--- scripts/helm-docs.sh
+++ scripts/helm-docs.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-## Reference: brew install norwoodj/tap/helm-docs
-helm-docs \
-    --chart-search-root=charts/codefresh \
-    --template-files=../../README.md.gotmpl \
-    --output-file=../../README.md
+## Reference: https://github.com/norwoodj/helm-docs
+set -x
+REPO_ROOT="${1:-$(cd "$(dirname "$0")/.." && pwd)}"
+WORKDIR="${2}"
+
+echo "$REPO_ROOT"
+echo "$WORKDIR"
+
+echo "Running Helm-Docs"
+docker run \
+    -v "$REPO_ROOT:/helm-docs" \
+    -w "/helm-docs/$WORKDIR" \
+    -u $(id -u) \
+    --rm \
+    --entrypoint /bin/sh \
+    jnorwood/helm-docs:v1.9.1 \
+    -c \
+    "helm-docs --chart-search-root=charts/codefresh --template-files=../../README.md.gotmpl --output-file=../../README.md"
